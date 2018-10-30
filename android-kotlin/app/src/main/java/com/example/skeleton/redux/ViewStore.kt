3bc2dbb7b9db016ba6442cff1401d3034f6664f2
cmh <blindoskar14@gmail.com>
2018-10-30 20:03:02 +0800

    #29 store for setting

--- android-kotlin/app/src/main/java/com/example/skeleton/redux/ViewStore.kt
+++ android-kotlin/app/src/main/java/com/example/skeleton/redux/ViewStore.kt
@@ -8,13 +8,17 @@ import redux.api.Store
 class ViewStore {
     // State
     data class State(
-            val agreeTermsConditions: Boolean = false
+            val agreeTermsConditions: Boolean = false,
+            val reminderOn: Boolean = false,
+            val strictModeOn: Boolean = false
     )
 
     // Actions
     sealed class Action {
         class _PresistenceRestore(val state: State) : Action()
         class AgreeTermsConditions : Action()
+        class setReminder(val on: Boolean) : Action()
+        class setStrictMode(val on: Boolean) : Action()
     }
 
     companion object {
@@ -25,7 +29,9 @@ class ViewStore {
         fun load(json: JSONObject?): State? {
             if (json == null) return null
             return State(
-                    agreeTermsConditions = json.getBoolean("agreeTermsConditions")
+                    agreeTermsConditions = json.getBoolean("agreeTermsConditions"),
+                    reminderOn = json.getBoolean("reminderOn"),
+                    strictModeOn = json.getBoolean("strictModeOn")
             )
         }
 
@@ -33,6 +39,8 @@ class ViewStore {
         fun save(state: State): JSONObject {
             return JSONObject()
                     .put("agreeTermsConditions", state.agreeTermsConditions)
+                    .put("reminderOn", state.reminderOn)
+                    .put("strictModeOn", state.strictModeOn)
         }
 
         // Reducer
@@ -43,6 +51,12 @@ class ViewStore {
                 }
                 is Action.AgreeTermsConditions -> {
                     state.copy(agreeTermsConditions = true)
+                }
+                is Action.setReminder -> {
+                    state.copy(reminderOn = action.on)
+                }
+                is Action.setStrictMode -> {
+                    state.copy(strictModeOn = action.on)
                 }
                 else -> state
             }
