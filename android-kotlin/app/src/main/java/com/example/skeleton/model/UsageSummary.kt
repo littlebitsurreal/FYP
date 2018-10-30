357b51377f86a2d2c0a23be185bc3e6020edc144
cmh <blindoskar14@gmail.com>
2018-10-30 20:01:02 +0800

    #27 implement UsageSummary

--- android-kotlin/app/src/main/java/com/example/skeleton/model/UsageSummary.kt
+++ android-kotlin/app/src/main/java/com/example/skeleton/model/UsageSummary.kt
@@ -1,8 +1,35 @@
 package com.example.skeleton.model
 
+import android.content.Context
+import com.example.skeleton.helper.PackageHelper
+import com.example.skeleton.iface.SerializableToJson
+import org.json.JSONObject
+
 data class UsageSummary(
         val appName: String,
         val packageName: String,
-        val useTimeTotal: Long,
-        val useTimeAverage: Long
-)
+        val useTimeTotal: Long
+) : SerializableToJson {
+    override fun toJson(): JSONObject {
+        return JSONObject()
+                .put("appName", appName)
+                .put("packageName", packageName)
+                .put("useTimeTotal", useTimeTotal)
+    }
+
+    companion object {
+        fun getSummary(context: Context, records: List<UsageRecord>): List<UsageSummary> {
+            val dict = hashMapOf<String, Long>()
+            for (r in records) {
+                dict[r.packageName] = (dict[r.packageName] ?: 0) + r.duration
+            }
+            return dict.map {
+                UsageSummary(PackageHelper.getAppName(context, it.key), it.key, it.value)
+            }.sortedByDescending { it.useTimeTotal }
+        }
+
+        fun fromJson(json: JSONObject): UsageSummary {
+            return UsageSummary(json.getString("appName"), json.getString("packageName"), json.getLong("useTimeTotal"))
+        }
+    }
+}
