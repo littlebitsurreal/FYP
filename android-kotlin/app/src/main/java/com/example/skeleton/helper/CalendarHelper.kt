cdd192a334ca4f4aa595b8630ca6733d54f5a180
cmh <blindoskar14@gmail.com>
2018-10-30 20:03:56 +0800

    #31 correct typo

--- android-kotlin/app/src/main/java/com/example/skeleton/helper/CalendarHelper.kt
+++ android-kotlin/app/src/main/java/com/example/skeleton/helper/CalendarHelper.kt
@@ -1,10 +1,11 @@
 package com.example.skeleton.helper
 
+import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
 
-object CalanderHelper {
+object CalendarHelper {
     fun getDate(time: Long): String {
         val cal = Calendar.getInstance(Locale.ENGLISH)
         cal.timeInMillis = time
@@ -19,5 +20,22 @@ object CalanderHelper {
         val cal = Calendar.getInstance(Locale.ENGLISH)
         cal.timeInMillis = time
         return SimpleDateFormat("yyyyMMdd").format(cal.time)
+    }
+    fun getTimeConsensed(time: Long): String {
+        val cal = Calendar.getInstance(Locale.ENGLISH)
+        cal.timeInMillis = time
+        return SimpleDateFormat("HH:mm").format(cal.time)
+    }
+    fun getDay(time: Long): String {
+        return DateFormat.getDateInstance(DateFormat.LONG).format(time)
+    }
+    fun getMonthDay(time: Long): String {
+        val cal = Calendar.getInstance(Locale.ENGLISH)
+        cal.timeInMillis = time
+        return SimpleDateFormat("dd/MM").format(cal.time)
+    }
+    fun toReadableDuration(duration: Long): String {
+        val t = duration / 1000
+        return if (t < 60) t.toString() + " sec" else (t / 60).toString() + " min"
     }
 }
