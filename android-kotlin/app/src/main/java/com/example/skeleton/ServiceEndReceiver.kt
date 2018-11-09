5d8aa870a02b103525c4824f34bf58a3a383cbca
cmh <blindoskar14@gmail.com>
2018-11-10 01:44:48 +0800

    #63 implemented service end receiver

---
+++ android-kotlin/app/src/main/java/com/example/skeleton/ServiceEndReceiver.kt
@@ -0,0 +1,19 @@
+package com.example.skeleton
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import com.example.skeleton.helper.Logger
+
+class ServiceEndReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        Logger.d("ServiceEndReceiver", "onReceive")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            context.startService(Intent(context, MyService::class.java))
+            // TODO: Change this
+        } else {
+            context.startService(Intent(context, MyService::class.java))
+        }
+    }
+}
