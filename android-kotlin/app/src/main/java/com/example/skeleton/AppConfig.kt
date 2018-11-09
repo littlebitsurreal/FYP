574e534c38bef710b24152187c9498603aad0593
cmh <blindoskar14@gmail.com>
2018-11-10 01:36:36 +0800

    #50 move constants into AppConfig

--- android-kotlin/app/src/main/java/com/example/skeleton/AppConfig.kt
+++ android-kotlin/app/src/main/java/com/example/skeleton/AppConfig.kt
@@ -2,7 +2,21 @@ package com.example.skeleton
 
 @Suppress("unused")
 object AppConfig {
-    const val SPLASH_DURATION = 0L // splash screen duration in ms, 0 to disable
-    const val SERVER_BASE = "http://10.0.2.2:8080/rest/"
-    const val TIMER_CHECK_PERIOD = 2000L
+    const val SPLASH_DURATION = 300L // splash screen duration in ms, 0 to disable
+    const val TIMER_CHECK_PERIOD = 3000L
+    const val TIMER_UPDATE_SERVER_PERIOD = 120000L // 2 minutes
+    const val MAIN_SCREEN_UPDATEVIEW_TIMER = 2500L
+    val SERVER_ADDRESS: String
+    const val SERVER_PORT = 8080
+
+    init {
+        when (BuildConfig.FLAVOR) {
+            "staging" -> {
+                SERVER_ADDRESS = "http://192.168.40.1:8080/"
+            }
+            else -> {
+                SERVER_ADDRESS = "http://192.168.40.1:8080/"
+            }
+        }
+    }
 }
