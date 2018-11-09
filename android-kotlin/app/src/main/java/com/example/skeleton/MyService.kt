a0dd86e37e8c6f3ba7c1a963154f15f10ef7e422
cmh <blindoskar14@gmail.com>
2018-11-10 01:45:28 +0800

    #64 make MyService consistant on Android O

--- android-kotlin/app/src/main/java/com/example/skeleton/MyService.kt
+++ android-kotlin/app/src/main/java/com/example/skeleton/MyService.kt
@@ -1,7 +1,5 @@
 package com.example.skeleton
 
-import android.app.job.JobParameters
-import android.app.job.JobService
 import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.content.Intent
@@ -10,61 +8,129 @@ import com.example.skeleton.helper.UsageStatsHelper
 import java.util.Timer
 import java.util.TimerTask
 import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.Service
+import android.content.IntentFilter
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.IBinder
 import android.os.SystemClock
+import android.support.v4.app.NotificationCompat
+import com.example.skeleton.client.UpdateServerTask
 import com.example.skeleton.helper.CalendarHelper
 import com.example.skeleton.helper.Logger
-import com.example.skeleton.helper.UsageStatsHelper.HOUR_24
+import com.example.skeleton.helper.NotTrackingListHelper
+import com.example.skeleton.helper.NotificationHelper
+import com.example.skeleton.helper.PackageHelper
 import com.example.skeleton.helper.UsageStatsHelper.queryTodayUsage
-import com.example.skeleton.model.UsageDigest
 import com.example.skeleton.model.UsageRecord
 import java.util.concurrent.locks.ReentrantLock
 
-class MyService : JobService() {
+/*
+ * different method to handle this class for Android version < 26 and >= 26
+ *
+ * Android version < 26
+ *      START_STICKY & restart using pending intent after service is killed
+ *
+ * Android version >= 26
+ *      register for foreground service
+ *      live-time register for Screen Unlock Receiver
+ */
+
+class MyService : Service() {
     private var mLastQueryTime: Long = 0
     private var mTimer: Timer? = null
-    private var usages = HashMap<String, Long>()
-    private var usageLimit = 1000 * 60 * 1
+    private var mTodayUsages = HashMap<String, Long>()
+    private var mNotTrackingList = listOf<String>()
+    private var mReminderOn = true
+    private var mStrictMode = true
+    private var mUsageLimit = 30 * 60 * 1000
 
-    // region Life cycle
-    override fun onStartJob(params: JobParameters?): Boolean {
-        Logger.d("MyService", "onStartJob")
-        return true
+    override fun onCreate() {
+        super.onCreate()
+        mNotTrackingList = NotTrackingListHelper.getNotTrackingList(this)
+        mUsageLimit = getSharedPreferences("MyService", Context.MODE_PRIVATE).getInt("mUsageLimit", 30 * 60 * 1000)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForeground()
+            registerScreenUnlockReceiver()
+        }
+        loadUsages()
+        startForegroundListener()
+        Logger.d("MyService", "onCreate")
     }
 
-    override fun onStopJob(params: JobParameters?): Boolean {
-        Logger.d("MyService", "onStopJob")
-        return true
+    override fun onDestroy() {
+        Logger.d("MyService", "onDestroy")
+        mTimer?.cancel()
+        sendBroadcast(Intent(this, ServiceEndReceiver::class.java))
+        unregisterReceiver(ScreenUnlockReceiver())
+        super.onDestroy()
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        Logger.d("MyService", "onBind")
+        return null
+    }
+
+    override fun onRebind(intent: Intent?) {
+        Logger.d("MyService", "onRebind")
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Logger.d("MyService", "onUnbind")
+        return super.onUnbind(intent)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         return START_STICKY
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        loadUsages()
-        startForegroundListener()
-
-        Logger.d("MyService", "onCreate")
-    }
-
-    override fun onDestroy() {
-        mTimer?.cancel()
-        Logger.d("MyService", "onDestroy")
-        super.onDestroy()
-    }
-
     override fun onTaskRemoved(rootIntent: Intent?) {
-        val restartService = Intent(applicationContext,
-                this.javaClass)
-        restartService.`package` = packageName
-        val restartServicePI = PendingIntent.getService(
-                applicationContext, 1, restartService,
-                PendingIntent.FLAG_ONE_SHOT)
-        val alarmService = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        alarmService.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 3000, restartServicePI)
+        Logger.d("MyService", "onTaskRemoved")
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            val restartService = Intent(applicationContext,
+                    this.javaClass)
+            restartService.`package` = packageName
+            val restartServicePI = PendingIntent.getService(
+                    applicationContext, 1, restartService,
+                    PendingIntent.FLAG_ONE_SHOT)
+            val alarmService = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            alarmService.set(AlarmManager.ELAPSED_REALTIME, SystemClock.elapsedRealtime() + 3000, restartServicePI)
+        }
         super.onTaskRemoved(rootIntent)
+    }
+    private fun startForeground() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+            val channel = NotificationChannel("default", "Channel Name", NotificationManager.IMPORTANCE_DEFAULT)
+            channel.description = "Channel Description"
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+            val icon = BitmapFactory.decodeResource(resources, R.drawable.elephant)
+
+            val notification = NotificationCompat.Builder(this, "default")
+                    .setContentTitle(resources.getString(R.string.app_name))
+                    .setContentText("Tracking App Usage")
+                    .setLargeIcon(icon)
+                    .setSmallIcon(R.drawable.notification_template_icon_low_bg)
+                    .setOngoing(true)
+                    .setContentIntent(pendingIntent)
+                    .build()
+            startForeground(System.currentTimeMillis().toInt() % 10000, notification)
+        }
+    }
+    private fun registerScreenUnlockReceiver() {
+        val intentFilter = IntentFilter()
+        intentFilter.addAction("android.intent.action.USER_PRESENT")
+        registerReceiver(ScreenUnlockReceiver(), intentFilter)
     }
     // endregion
 
@@ -72,34 +138,34 @@ class MyService : JobService() {
         val timer = Timer(true)
         var lastForegroundPackageName: String? = null
         val usageStatsManager = getSystemService(AppCompatActivity.USAGE_STATS_SERVICE) as UsageStatsManager
-        val prefEdit = getSharedPreferences("MyService", Context.MODE_PRIVATE).edit()
-        val pref = getSharedPreferences("MyService", Context.MODE_PRIVATE)
 
         mTimer = timer
-        mLastQueryTime = getSharedPreferences("MyService", Context.MODE_PRIVATE).getLong("mLastQueryTime", 0L)
+        mLastQueryTime = getSharedPreferences("MyService", Context.MODE_PRIVATE).getLong("mLastQueryTime", 1000L)
 
         val monitoringTask = object : TimerTask() {
             val lock = ReentrantLock()
 
             override fun run() {
                 if (lock.tryLock()) {
-                    val currentTime = System.currentTimeMillis()
-                    val records = UsageStatsHelper.getLatestEvent(this@MyService, usageStatsManager, mLastQueryTime, currentTime)
-                    val foregroundEvent = if (records.isEmpty()) {
+                    val result = UsageStatsHelper.getLatestEvent(this@MyService, usageStatsManager, mLastQueryTime, System.currentTimeMillis())
+                    val foregroundEvent = if (result.records.isEmpty()) {
                         null
                     } else {
-                        records.last()
+                        result.records.last()
                     }
 
-                    mLastQueryTime = currentTime
-                    prefEdit.putLong("mLastQueryTime", mLastQueryTime)
-                    prefEdit.apply()
+                    if (result.lastEndTime != 0L) {
+                        mLastQueryTime = result.lastEndTime
+                        val prefEdit = getSharedPreferences("MyService", Context.MODE_PRIVATE).edit()
+                        prefEdit.putLong("mLastQueryTime", mLastQueryTime)
+                        prefEdit.commit()
+                    }
 
-                    addUsages(records)
+                    addUsages(result.records)
 
                     if (foregroundEvent != null) {
                         if (lastForegroundPackageName != foregroundEvent.packageName) {
-                            notifyAppChange(foregroundEvent.packageName)
+                            onAppSwitch(foregroundEvent.packageName)
                         }
                         lastForegroundPackageName = foregroundEvent.packageName
                     }
@@ -107,57 +173,32 @@ class MyService : JobService() {
                 lock.unlock()
             }
         }
-        val updateServerTask = object : TimerTask() {
-            override fun run() {
-                val day = CalendarHelper.getDayCondensed(System.currentTimeMillis() - HOUR_24)
-                val updatedDays = pref.getStringSet("updated", setOf<String>()).toMutableSet()
-                if (!updatedDays.contains(day)) {
-                    val digest = UsageDigest.load(this@MyService, day)
-                    if (digest.totalTime == 0L) {
-                        updatedDays.add(day)
-                        prefEdit.putStringSet("updated", updatedDays)
-                        prefEdit.apply()
-                        Logger.d("updateServerTask", "empty record")
-                    } else {
-                        // TODO: update server
-                        var success = true
-                        if (success) {
-                            updatedDays.add(day)
-                            prefEdit.putStringSet("updated", updatedDays)
-                            prefEdit.apply()
-                            Logger.d("updateServerTask", "success")
-                        } else {
-                            Logger.d("updateServerTask", "failed")
-                        }
-                    }
-                }
-                val digest = UsageDigest
-            }
-        }
+        val updateServerTask = UpdateServerTask(this)
         timer.schedule(monitoringTask, 0, AppConfig.TIMER_CHECK_PERIOD)
-        timer.schedule(updateServerTask, 10000, 60 * 1000)
+        timer.schedule(updateServerTask, 10000, AppConfig.TIMER_UPDATE_SERVER_PERIOD)
     }
 
-    private fun notifyAppChange(packageName: String) {
-        var reminderOn = true
-        var strictMode = true
-        var notTrackingList = listOf<String>()
+    private fun onAppSwitch(packageName: String) {
+        val t = mTodayUsages[packageName] ?: 0
+        Logger.d("onAppSwitch", "$packageName - usage: ${t
+                / 60000}min  limit: ${mUsageLimit / 60000}  In NotTrackingList: ${mNotTrackingList.contains(packageName)}")
 
-        if (usages[packageName] ?: 0 >= usageLimit) {
-            Logger.d("notifyAppChange", "$packageName exceed usage limit")
+        if (t >= mUsageLimit && !mNotTrackingList.contains(packageName)) {
+            NotificationHelper.show(this, "Usage Monitor",
+                    "You have used ${PackageHelper.getAppName(this, packageName)} for ${CalendarHelper.toReadableDuration(t)}", packageName.hashCode())
         }
     }
 
     private fun loadUsages() {
         val records = queryTodayUsage(this)
         for (r in records) {
-            usages[r.packageName] = (usages[r.packageName] ?: 0) + r.duration
+            mTodayUsages[r.packageName] = (mTodayUsages[r.packageName] ?: 0) + r.duration
         }
     }
 
     private fun addUsages(records: List<UsageRecord>) {
         for (r in records) {
-            usages[r.packageName] = (usages[r.packageName] ?: 0) + r.duration
+            mTodayUsages[r.packageName] = (mTodayUsages[r.packageName] ?: 0) + r.duration
         }
     }
 }
